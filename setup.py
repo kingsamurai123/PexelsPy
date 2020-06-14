9ea0a9cac828312e62091748628188c34952bf10
Sravan Kumar <42110198+kingsamurai123@users.noreply.github.com>
2020-06-14 15:40:58 +0530

    Regular push
    Classifiers

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="pexelsPy",
-    version="1.0.2",
+    version="1.0.0",
     author="Sravan Kumar Shaganti",
     author_email="demonlyf98@protonmail.com",
     description="Use Pexels API v1 with Python",
@@ -18,21 +18,8 @@ setuptools.setup(
     
 )
 """
-Previous repo details
-
 setuptools.setup(
-    name="pexels-api",
-    version="1.0.1",
-    author="Arturo Aguilar Lagunas",
-    author_email="aguilar.lagunas.arturo@gmail.com",
-    description="Use Pexels API v1 with Python",
-    long_description=long_description,
-    long_description_content_type="text/markdown",
-    url="https://github.com/AguilarLagunasArturo/pexels-api",
-    keywords='pexels api images photos',
-    install_requires=['requests'],
-    packages=setuptools.find_packages(),
-    classifiers=[
+classifiers=[
         "Programming Language :: Python",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
