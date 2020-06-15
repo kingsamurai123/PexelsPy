3828081fad86f445372dc8fd31233292db14e975
Sravan Kumar <42110198+kingsamurai123@users.noreply.github.com>
2020-06-15 21:24:43 +0530

    New Version
    Add new version number to the setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="pexelsPy",
-    version="1.0.0",
+    version="1.0.1",
     author="Sravan Kumar",
     author_email="demonlyf98@protonmail.com",
     description="Use Pexels API v1 with Python",
