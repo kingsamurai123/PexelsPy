59159b7cc21ca2dde2336a41459d123e9a7643f0
Sravan Kumar <42110198+kingsamurai123@users.noreply.github.com>
2020-06-19 09:40:34 +0530

    Changed the details to log files
    Instead of displaying the results in the terminal itself. Now a separate directory is created in the home directory and the results are stored there.

--- pexelsPy/singleSamples/photos/curatedPhotos.py
+++ pexelsPy/singleSamples/photos/curatedPhotos.py
@@ -1,11 +1,22 @@
 from pexelsPy import API
 import os
 import sys
+from datetime import datetime
+
 # Init api object with your Pexels API key
 API_KEY = os.environ.get("PEXELS_API")
 api = API(API_KEY)
 page_number = sys.argv[1]
 results_in_a_page = sys.argv[2]
+
+#date initialize
+date = datetime.now()
+
+#directory variables
+directory_name = date.strftime("%b-%d-%Y")   #directory name is same as the execution date
+filename = date.strftime("%H-%M-%S")
+filename = (filename+" curated.txt")
+home = os.environ.get("HOME")    #to retrieve the home directory using inbuilt home variable
 
 #Page number should be >= 1 or a response errror 500 is returned. Sooooo..
 if page_number == 0:
@@ -15,37 +26,43 @@ if page_number == 0:
 if results_in_a_page == 0:
     results_in_a_page = 1
 
-# Search curated photos in the fifth page
+#Directory allocation and changing directory
+if os.path.exists(home+'/'+directory_name):
+    print("    ----------------------   Directory already exists  ---------------")
+else:
+    os.mkdir(home+'/'+directory_name)
+    print("    ----------------------   New Directory is created  ---------------")
+os.chdir(home+'/'+directory_name)
+print("The Directory Location is: "+os.getcwd())
+
+#Start the file handler
+file = open(filename,"w+")
+
+
+# Search popular photos in the fifth page
 api.curated_photos(results_per_page = results_in_a_page, page = page_number)
-# Loop backwards
+
+# Main loop
 for _ in page_number:
     # Get all photos in the page
     photos = api.get_photos()
     # For each photo print its properties
     for photo in photos:
-        print("-----------------------------------------------")
-        print("Photo id: ", photo.id)
-        print("Photo width: ", photo.width)
-        print("Photo height: ", photo.height)
-        print("Photo url: ", photo.url)
-        print("Photographer: ", photo.photographer)
-        print("Photo description: ", photo.description)
-        print("Photo extension: ", photo.extension)
-        print("Photo sizes:")
-        print("\toriginal: ", photo.original)
-        print("\tcompressed: ", photo.compressed)
-        print("\tlarge2x: ", photo.large2x)
-        print("\tlarge: ", photo.large)
-        print("\tmedium: ", photo.medium)
-        print("\tsmall: ", photo.small)
-        print("\ttiny: ", photo.tiny)
-        print("\tportrait: ", photo.portrait)
-        print("\tlandscape: ", photo.landscape)
-        print("-----------------------------------------------\n")
-    # print("Page Number: ",api.page)
-    # If there is no previous page print the first page and end the loop
-    # if not api.has_previous_page:
-    #     print("First page: ", api.page)
-    #     break
-    # Search previous page
-    # api.search_previous_page()
+        file.write("Photo ID: {}\n".format(photo.id))
+        file.write("Photo width: {}\n".format(photo.width))
+        file.write("Photo URL: {}\n".format(photo.height))
+        file.write("Photo URL: {}\n".format(photo.url))
+        file.write("Photo URL: {}\n".format(photo.photographer))
+        file.write("Photo URL: {}\n".format(photo.description))
+        file.write("Photo URL: {}\n".format(photo.extension))
+        file.write("Photo Sizes\n")
+        file.write("\tOriginal: {}\n".format(photo.original))
+        file.write("\tCompressed: {}\n".format(photo.compressed))
+        file.write("\tLarge2x: {}\n".format(photo.large2x))
+        file.write("\tLarge: {}\n".format(photo.large))
+        file.write("\tMedium: {}\n".format(photo.medium))
+        file.write("\tSmall: {}\n".format(photo.small))
+        file.write("\tTiny: {}\n".format(photo.tiny))
+        file.write("\tPortrait: {}\n".format(photo.portrait))
+        file.write("\tLandscape: {}\n\n\n".format(photo.landscape))
+file.close()
--- pexelsPy/singleSamples/photos/searchPhotos.py
+++ pexelsPy/singleSamples/photos/searchPhotos.py
@@ -1,14 +1,25 @@
 from pexelsPy import API
+from datetime import datetime
 import os
 import sys
 # Init api object with your Pexels API
 API_KEY = os.environ.get("PEXELS_API")
 api = API(API_KEY)
 
+
+#date initialize
+date = datetime.now()
+
 # Search Photo Variables
 keyWord = str(sys.argv[1])
 page_number = sys.argv[2]
 results_in_a_page = sys.argv[3]
+
+#directory variables
+directory_name = date.strftime("%b-%d-%Y")   #directory name is same as the execution date
+filename = date.strftime("%H-%M-%S")
+filename = (filename+" search.txt")
+home = os.environ.get("HOME")    #to retrieve the home directory using inbuilt home variable
 
 #Page number should be >= 1 or a response errror 500 is returned. Sooooo..
 if page_number == 0:
@@ -18,37 +29,42 @@ if page_number == 0:
 if results_in_a_page == 0:
 	results_in_a_page = 1
 
+#Directory allocation and changing directory
+if os.path.exists(home+'/'+directory_name):
+	print("    ----------------------   Directory already exists  ---------------")
+else:
+	os.mkdir(home+'/'+directory_name)
+	print("    ----------------------   New Directory is created  ---------------")
+os.chdir(home+'/'+directory_name)
+print("The Directory Location is: "+os.getcwd())
 
+#Start the file handler
+file = open(filename,"w+")
+
+#Actual photos search
 api.search_photos(keyWord, page=page_number , results_per_page = results_in_a_page)
-print("Total results: ", api.total_results)
+# print("Total results: ", api.total_results)
 # Loop all the pages
 for _ in page_number:
-    # Get all photos in the page
-    photos = api.get_photos()
-    # For each photo print its properties
-    for photo in photos:
-        print("-----------------------------------------------")
-        print("Photo id: ", photo.id)
-        print("Photo width: ", photo.width)
-        print("Photo height: ", photo.height)
-        print("Photo url: ", photo.url)
-        print("Photographer: ", photo.photographer)
-        print("Photo description: ", photo.description)
-        print("Photo extension: ", photo.extension)
-        print("Photo sizes:")
-        print("\toriginal: ", photo.original)
-        print("\tcompressed: ", photo.compressed)
-        print("\tlarge2x: ", photo.large2x)
-        print("\tlarge: ", photo.large)
-        print("\tmedium: ", photo.medium)
-        print("\tsmall: ", photo.small)
-        print("\ttiny: ", photo.tiny)
-        print("\tportrait: ", photo.portrait)
-        print("\tlandscape: ", photo.landscape)
-        print("-----------------------------------------------\n")
-    # If there is no next page print the last page and end the loop
-    # if not api.has_next_page:
-    #     print("Last page: ", api.page)
-    #     break
-    # # Search next page
-    # api.search_next_page()
+	# Get all photos in the page
+	photos = api.get_photos()
+	# For each photo print its properties
+	for photo in photos:
+		file.write("Photo ID: {}\n".format(photo.id))
+		file.write("Photo width: {}\n".format(photo.width))
+		file.write("Photo URL: {}\n".format(photo.height))
+		file.write("Photo URL: {}\n".format(photo.url))
+		file.write("Photo URL: {}\n".format(photo.photographer))
+		file.write("Photo URL: {}\n".format(photo.description))
+		file.write("Photo URL: {}\n".format(photo.extension))
+		file.write("Photo Sizes\n")
+		file.write("\tOriginal: {}\n".format(photo.original))
+		file.write("\tCompressed: {}\n".format(photo.compressed))
+		file.write("\tLarge2x: {}\n".format(photo.large2x))
+		file.write("\tLarge: {}\n".format(photo.large))
+		file.write("\tMedium: {}\n".format(photo.medium))
+		file.write("\tSmall: {}\n".format(photo.small))
+		file.write("\tTiny: {}\n".format(photo.tiny))
+		file.write("\tPortrait: {}\n".format(photo.portrait))
+		file.write("\tLandscape: {}\n\n\n".format(photo.landscape))
+file.close()
